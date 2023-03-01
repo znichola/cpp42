03185f6656fbe521043cc091601ac4de38fcfe6c
znichola <niki.zivkovic@hotmail.com>
2023-03-01 22:59:11 +0100

    it's working

--- m00/ex00/megaphone.cpp
+++ m00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 18:34:01 by znichola          #+#    #+#             */
-/*   Updated: 2023/03/01 20:17:19 by znichola         ###   ########.fr       */
+/*   Updated: 2023/03/01 22:58:19 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,18 @@ int	main(int ac, char **av)
 	}	
 	for (int i = 1; i < ac; i++)
 	{
+		auto& f = std::use_facet<std::ctype<wchar_t>>(std::locale());
+		std::wstring str = L"yes";
 		std::string s(av[i]);
-		std::cout << s.size();
-		for (int i = 0; i < s.size(); i++)
-		{
-			wchar_t	c = s[i];
-			std::cout << c.toupper();
-		}
-	//	for (int i = 0; av[i].size(); i++
-	//	std::cout << std::toupper((av[i])) << std::endl;
+		//std::wstring foo(av[i]);
+		//std::wstring foo = av[i];
+		auto& b = std::use_facet<std::ctype<char>>(std::locale());
+		std::string foo(av[i]);
+		b.toupper(&foo[0], &foo[0] + foo.size());
+		
+		f.toupper(&str[0], &str[0] + str.size());
+		
+		//std::wcout << str << std::endl;
+		std::cout << foo << std::endl;
 	}		
 }
