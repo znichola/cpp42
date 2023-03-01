909632412eb9fbeceff84bae4f1d7af744547ed5
znichola <niki.zivkovic@hotmail.com>
2023-03-01 18:48:31 +0100

    basic makefile

--- m00/ex00/megaphone.cpp
+++ m00/ex00/megaphone.cpp
@@ -6,7 +6,22 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 18:34:01 by znichola          #+#    #+#             */
-/*   Updated: 2023/02/28 18:34:01 by znichola         ###   ########.fr       */
+/*   Updated: 2023/03/01 18:15:36 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}	
+	for (int i = 1; i < ac; i++)
+	{
+		//std::cout << std::toupper((av[i])) << std::endl;
+	}		
+}
