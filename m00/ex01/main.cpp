9b99fd8dfa71ce6d3e4ac9fd7b13acf5e317a6ad
znichola <niki.zivkovic@hotmail.com>
2023-03-08 12:05:20 +0100

    main readjust

--- m00/ex01/main.cpp
+++ m00/ex01/main.cpp
@@ -6,17 +6,22 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 04:58:14 by znichola          #+#    #+#             */
-/*   Updated: 2023/03/06 05:01:38 by znichola         ###   ########.fr       */
+/*   Updated: 2023/03/07 22:42:03 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include "phonebook.hpp"
 
 int	main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-	std::cout<< "hello world" << std::endl;
-}
+	char	buff[512];
 
+	std::cout << "Hello PhoneBook!" << std::endl;
+	std::cout << "Enter info: ";
+	std::cin >> buff;
+	std::cout << "[" << buff << "]" << std::endl;
+}
