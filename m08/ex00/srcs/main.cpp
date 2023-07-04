f7bc8e7d359f7891c8c20f2f6c66ff4371cac4d3
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-07-04 20:13:34 +0200

    changes before final push

--- m08/ex00/srcs/main.cpp
+++ m08/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 12:30:57 by znichola          #+#    #+#             */
-/*   Updated: 2023/07/02 14:09:45 by znichola         ###   ########.fr       */
+/*   Updated: 2023/07/04 16:05:47 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,29 @@ int	main(void)
 		}
 
 	}
+	{
+		std::deque<int> foo(7);
 
+		for (int i = 0; i < 7; i++)
+		{
+			foo[i] = i * i;
+			std::cout << foo[i] << " ";
+		}
+
+		std::cout << std::endl;
+
+		std::cout << *easyfind(foo, 16) << std::endl;
+
+		try
+		{
+			std::cout << *easyfind(foo, 13);
+			std::cout << " found 13" << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "could not find 13 : " <<  e.what() << std::endl;
+		}
+
+	}
 	return(0);
 }
