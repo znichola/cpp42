698eefd4bbdaa04a7b90cf21c42f7085b2d68c6f
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-07-06 14:13:19 +0200

    RNP edgecase fix
    - and it uses RNP, oh my god.

--- m09/ex01/srcs/RPN.cpp
+++ m09/ex01/srcs/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 21:30:41 by znichola          #+#    #+#             */
-/*   Updated: 2023/07/04 23:38:43 by znichola         ###   ########.fr       */
+/*   Updated: 2023/07/06 14:12:25 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,38 +28,36 @@ void RPN::solve(const std::string &args)
 {
 	std::stringstream ss(args);
 	std::stack<int> stack;
-	char tok;
+	std::string tok;
 
 	while (ss >> tok)
 	{
+		if (tok.size() != 1)
+			goto error_out;
 		int n, x, y = 0;
-		if (!std::isdigit(tok))
+		if (!std::isdigit(tok[0]))
 		{
 			if (stack.size() < 2)
-			{
-				std::cerr << "Error" << std::endl;
-				return ;
-			}
+				goto error_out;
 			y = stack.top();
 			stack.pop();
 			x = stack.top();
 			stack.pop();
-			if      (tok == '+') n = x + y;
-			else if (tok == '-') n = x - y;
-			else if (tok == '*') n = x * y;
-			else if (tok == '/' && y != 0) n = x / y;
-			else
-			{
-				std::cerr << "Error" << std::endl;
-				return ;
-			}
+			if      (tok == "+") n = x + y;
+			else if (tok == "-") n = x - y;
+			else if (tok == "*") n = x * y;
+			else if (tok == "/" && y != 0) n = x / y;
+			else	goto error_out;
 		}
 		else
-			n = tok - '0';
+			n = tok[0] - '0';
 		stack.push(n);
 	}
 	if (stack.size() != 1)
+	{
+		error_out:
 		std::cerr << "Error" << std::endl;
+	}
 	else
 		std::cout << stack.top() << std::endl;
 }
