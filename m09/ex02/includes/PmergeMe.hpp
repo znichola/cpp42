0fdb2051943ed6390c2d3869a62413b0e24449b2
znichola <niki.zivkovic@hotmail.com>
2023-07-12 16:12:54 +0200

    timing print format is all screwy

--- m09/ex02/includes/PmergeMe.hpp
+++ m09/ex02/includes/PmergeMe.hpp
@@ -15,6 +15,8 @@
 # include <iostream>
 # include <sstream>
 # include <cstdlib>
+# include <sys/time.h>
+# include <iomanip>
 
 template<typename T>
 class PmergeMe
@@ -28,13 +30,13 @@ public:
 
 	~PmergeMe() {}
 	
-	PmergeMe(int ac, char **av)
+	void init(int ac, char **av)
 	{
 		for (int i = 0; i < ac; i++)
 			_elements.push_back(std::atoi(av[i]));
 	}
 
-	PmergeMe(std::string args)
+	void init(std::string args)
 	{
 			std::cout << "string input\n";
 		int x = 0;
@@ -48,16 +50,38 @@ public:
 
 	PmergeMe & operator=(const PmergeMe &other) { _elements = other._elements; return *this; }
 
-	void print() const
+	void print(std::string sep) const
 	{
-		std::cout << "printing Pmerge\n";
 		for (typename T::const_iterator it = _elements.begin(); it < _elements.end(); it++)
 		{
 			if (it != _elements.begin())
-				std::cout << ", ";
+				std::cout << sep;
 			std::cout << *it;
 		}
 		std::cout << std::endl;
+	}
+
+	void before() const { std::cout << "Before: "; print(" "); }
+
+	void after()  const { std::cout << "After:  "; print(" "); }
+
+	void time(const std::string &cont)
+	{
+		struct timeval start, end;
+		std::cout << std::setprecision(5);
+		std::cout << "Time to process range of " << _elements.size()
+		<< " elements with std::" << cont << " : ";
+		gettimeofday(&start,  0);
+		sort();
+		gettimeofday(&end, 0);
+		std::cout << end.tv_usec - start.tv_usec / 100000.0f << " us" << std::endl;
+		std::cout << 0.000001f << " us" << std::endl;
+
+	}
+	void sort()
+	{
+		for (int i = 0; i < 100000; i++)
+			;
 	}	
 };
 
--- m09/ex02/srcs/main.cpp
+++ m09/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 11:53:03 by znichola          #+#    #+#             */
-/*   Updated: 2023/07/10 17:09:57 by znichola         ###   ########.fr       */
+/*   Updated: 2023/07/12 06:10:56 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,21 @@
 
 int	main(int ac, char **av)
 {
-	(void)ac;
-	(void)av;
 	std::cout << "hello PmergeMe!" << std::endl;
+	if (ac < 2)
+	{
+		std::cout << "give me some arguments" << std::endl;
+		return 1;
+	}	
+	PmergeMe<std::vector<int> >foo;
 
-	PmergeMe<std::vector<int> >foo("1 2 3 4 41 42 -8       69      420");
+	if (ac == 2)	
+		foo.init(std::string(av[1]));
+	else
+		foo.init(ac - 1, av + 1);
 
-	foo.print();	
+	foo.before();
+	foo.after();	
+	foo.time("vector");
 	return(0);
 }
